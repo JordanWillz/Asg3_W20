4e6e0413868a096e1f4c512f98969339a0fc6857
Jordan Williams <jordanwilliams@localhost.localdomain>
2020-03-08 22:33:02 -0600

    finishing up

--- index.js
+++ index.js
@@ -5,68 +5,142 @@ var random_name = require('node-random-name');
 
 let messages = [];
 let users = [];
+let clr_exp = new RegExp('[0-9A-Fa-f]{6}|[0-9A-Fa-f]{3}'); //color reg expression
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+function get_user(id)
+  {
+    for(let i = 0; i < users.length; i++){
+      console.log(users[i].id);
+      console.log(id);
+      if(users[i].id === id){
+        console.log("user found");
+        return users[i];
+      }
+    }
+    console.log("unable to find user");
+    return null;
+  }
+
+
 io.on('connection', function(socket){
   
   function get_username()
   {
+    return users.username;
+  }
+
+
+function get_id()
+  {
     let cookies = socket.handshake.headers.cookie;
-    let username = random_name();
-    if(cookies.includes("nickname="))
+    let id = Math.floor(Math.random() * 53232);
+    if(cookies.includes("user_id="))
     {
-      username = cookies.substr(cookies.indexOf("nickname=") + 9);
-      username = username.substr(0, username.indexOf(";"));
-      console.log("User with nickname connected: ", username);
+      id = cookies.substr(cookies.indexOf("user_id") + 8);
+      id = id.substr(0, id.indexOf(";"));
+      console.log("User with id connected: ", id);
     }
     else{
-      socket.emit('username', username);
+      socket.emit('user_id', id);
     }
-    return username;
+    return parseInt(id);
   }
-  
-  let user = get_username(true);
-  users.push(user);
-  console.log(user ,' connected');
+
+
+  let id = get_id();
+  let found = false;
+  for(let i = 0; i < users.length; i++){
+      if(users[i].id === get_id()){
+        users[i].connected = true;
+        console.log("user reconnected");
+        found = true;
+      }
+  }
+  if(!found)
+  {
+    let user = {
+      'id': id,
+      'username': "guest"+id,
+      'color': "#00008b",
+      'connected': true,
+    }
+    users.push(user);
+  }
+  //console.log(user ,' connected');
   io.emit('user update', users);
   console.log(users);
   
   
   //send the message log from previous connections
   for (index in messages)
-  {  	
+  {
   	console.log('sending message', messages[index]);
   	socket.emit('chat message', messages[index]);	
   }
   
   socket.on('disconnect', function(){
-    console.log(users);
-    let user_ind = users.indexOf(get_username());
-    users.splice(user_ind,user_ind + 1);
-  	//io.emit('user dis', get_username());
-    console.log('user dis', get_username());
-    //io.emit('user update', users);
+    console.log("USERS: "+ users[0].id);
+    console.log("USER ID:" + get_id());
+
+    for(let i = 0; i < users.length; i++){
+      if(users[i].id === get_id()){
+        users[i].connected = false;
+        console.log("user disconnected");
+      }
+      io.emit('user update', users);
+    }
   });
   
 });
 
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(messg, usr){
+  socket.on('chat message', function(messg, id){
   	let tm = Date.now();
-  	
-  	//format message as object with time and message
-  	let message = {time: tm, 
-    msg: messg,
-    user: usr, 
-  	};
-  	
-  	messages.push(message);
-  	console.log(message);
-  	io.emit('chat message', message);
+    let user = get_user(parseInt(id));
+
+    if(messg.includes("/nick ")){
+      console.log("user attempting to change username");
+      let nick = messg.substr(6);
+      if(users.some(_user => _user.username === nick && _user.connected)){
+        socket.emit("Error", "Nickname already taken!");
+      }
+      else
+      {
+        user.username = nick;
+        io.emit('user update', users);
+      }
+    }
+    else if(messg.includes("/nickcolor ")){
+      console.log("user attempting to change nick color");
+      let color = messg.substring(11);
+      if(clr_exp.test(color)){
+        user.color = "#"+color;
+        io.emit('user update', users);
+      }
+      else{
+        socket.emit('Error', "Color code not recognized (please use hex color code format)");
+        console.log("Color code format not correct");
+      }
+    }
+    else{
+  	  //format message as object with time and message
+  	  let message = {
+        time: tm,
+        msg: messg,
+        user: id,
+        username: user.username,
+        color: user.color
+      };
+
+  	  messages.push(message);
+  	  console.log(message);
+  	  io.emit('chat message', message);
+    }
     //io.emit('chat message - content', msg);
   });
 });
